8f83d42da2873c1d931cf64b2de2a8506932ee37
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-05-19 15:24:21 +0200

    Update

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  id;
+  contact;
+
+  // On inject un object de la class ActivatedRoute
+  constructor(private activatedRoute:ActivatedRoute,
+    private contactService:ContactService) { }
 
   ngOnInit(): void {
+
+    // 1/ Récupérer l'id dans l'url ActivatedRoute
+    console.log(this.activatedRoute.snapshot.params);
+    this.activatedRoute.snapshot.params.id;  // {id: 1, name: 'Fred'}
+
+    // 2/ Récupérer le bon object dans dbContact (dans le service)
+    this.contactService.getContacts()[this.id];
+
+    // 3/ 
   }
 
 }
